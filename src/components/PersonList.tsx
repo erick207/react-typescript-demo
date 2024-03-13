d9e7177f12ad61c80bebc8370078089d9eecbf31
Erick <12631133+erick207@users.noreply.github.com>
2024-03-13 14:56:03 -0300

    initial commit

---
+++ src/components/PersonList.tsx
@@ -0,0 +1,21 @@
+type PersonListProps = {
+  // Array of objects is specified as an object literal followed by square brackets
+  names: {
+    first: string
+    last: string
+  }[]
+}
+
+export const PersonList = (props: PersonListProps) => {
+  return (
+    <div>
+      {props.names.map((name) => {
+        return (
+          <h2 key={name.first}>
+            {name.first} {name.last}
+          </h2>
+        )
+      })}
+    </div>
+  )
+}
