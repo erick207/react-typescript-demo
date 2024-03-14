afe69563d8ef3e88a26de752bab4be9a46f2cd72
Erick <12631133+erick207@users.noreply.github.com>
2024-03-14 16:38:45 -0300

    css styles as props

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import './App.css'
 import { Button } from './components/Button'
+import { Container } from './components/Container'
 import { Input } from './components/Input'
 
 function App() {
   return (
     <div className='App'>
-      <Button
-        handleClick={(event, id) => {
-          console.log('Button clicked', event, id)
-        }}
+      <Container
+        styles={{ border: '10px dashed peachpuff', padding: '1rem' }}
       />
-      <Input value='' handleChange={(event) => console.log(event)} />
     </div>
   )
 }
