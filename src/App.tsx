8446e65e74935783628b402df6e6ed56da61656a
Erick <12631133+erick207@users.noreply.github.com>
2024-03-14 13:06:26 -0300

    click event as component prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import './App.css'
-import { Greet } from './components/Greet'
-import { Heading } from './components/Heading'
-import { Oscar } from './components/Oscar'
-import { Status } from './components/Status'
+import { Button } from './components/Button'
 
 function App() {
   return (
     <div className='App'>
-      <Status status='loading' />
-      <Heading>Placeholder Text</Heading>
-      <Oscar>
-        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
-      </Oscar>
-      <Greet name='Erick' isLoggedIn={true} messageCount={8} />
+      <Button
+        handleClick={(event, id) => {
+          console.log('Button clicked', event, id)
+        }}
+      />
     </div>
   )
 }
