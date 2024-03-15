09c644b8cea12d76d52ed6b8b5b317365af4b1ec
Erick <12631133+erick207@users.noreply.github.com>
2024-03-15 11:56:59 -0300

    useState Hook  and type inference of basic types

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,7 @@ import './App.css'
 import { Container } from './components/Container'
 
 function App() {
-  return (
-    <div className='App'>
-      <Container
-        styles={{ border: '10px dashed peachpuff', padding: '1rem' }}
-      />
-    </div>
-  )
+  return <div className='App'></div>
 }
 
 export default App
