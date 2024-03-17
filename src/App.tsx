c4bab536e689d0f4b04ea98822343c89ddabd9ce
Erick <12631133+erick207@users.noreply.github.com>
2024-03-17 11:39:15 -0300

    component prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css'
+import { Private } from './components/auth/Private'
+import { Profile } from './components/auth/Profile'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <Private isLoggedIn={true} component={Profile} />
+    </div>
+  )
 }
 
 export default App
