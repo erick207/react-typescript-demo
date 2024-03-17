6b7bbfe33b0146a4646f911eb2b7bb5f8a4e5551
Erick <12631133+erick207@users.noreply.github.com>
2024-03-17 10:12:36 -0300

    class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css'
+import { Counter } from './components/class/Counter'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <Counter message='The count value is '></Counter>
+    </div>
+  )
 }
 
 export default App
