fea42073c9c168524aa9e96f8a5d4c3484e0cdae
Erick <12631133+erick207@users.noreply.github.com>
2024-03-17 11:40:29 -0300

    clear App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import './App.css'
-import { Private } from './components/auth/Private'
-import { Profile } from './components/auth/Profile'
 
 function App() {
-  return (
-    <div className='App'>
-      <Private isLoggedIn={true} component={Profile} />
-    </div>
-  )
+  return <div className='App'></div>
 }
 
 export default App
