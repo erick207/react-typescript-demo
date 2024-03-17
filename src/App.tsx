132b54290677ec229269d2f2241e173dd2e8e1cb
Erick <12631133+erick207@users.noreply.github.com>
2024-03-17 18:45:23 -0300

    clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import { List } from './components/generics/List'
 import './App.css'
 
 function App() {
-  return (
-    <div className='App'>
-      <List
-        items={['Batman', 'Superman', 'Wonder Woman']}
-        onClick={(item) => console.log(item)}
-      />
-      <List items={[1, 2, 3]} onClick={(item) => console.log(item)} />
-      <List
-        items={[
-          { first: 'Bruce', last: 'Wayne' },
-          { first: 'Clark', last: 'Kent' },
-          { first: 'Princess', last: 'Diana' },
-        ]}
-        onClick={(item) => console.log(item)}
-      />
-    </div>
-  )
+  return <div className='App'></div>
 }
 
 export default App
