b94dd9c2b8399a287a61404fc1489921d23e49fc
Erick <12631133+erick207@users.noreply.github.com>
2024-03-18 10:09:24 -0300

    Restricting Props: before restricting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css'
+import { RandomNumber } from './components/restriction/RandomNumber'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <RandomNumber value={10} isPositive isNegative isZero />
+    </div>
+  )
 }
 
 export default App
