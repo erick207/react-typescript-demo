9489f431c49bbab81c8447dbfa7517fb96a20059
Erick <12631133+erick207@users.noreply.github.com>
2024-03-18 09:40:48 -0300

    generic props - explain extra cases in comment

--- src/components/generics/List.tsx
+++ src/components/generics/List.tsx
@@ -3,7 +3,8 @@ type ListProps<T> = {
   onClick: (value: T) => void
 }
 
-// if we want to be more specific on types we can replace this for <T extends string | null>:
+// if we want to be more specific on types we can replace this for <T extends string | number>:
+// another useful constraint is mentioning each object must contain an id property: <T extends {id: number}>, then we would return <div key={item.id}....
 export const List = <T extends {}>({ items, onClick }: ListProps<T>) => {
   return (
     <div>
