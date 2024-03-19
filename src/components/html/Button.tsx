67251ef4f03b3f73b2fffbc600af7a15fc7e8dfb
Erick <12631133+erick207@users.noreply.github.com>
2024-03-19 14:53:34 -0300

    Restricting children type using Omit keyword

--- src/components/html/Button.tsx
+++ src/components/html/Button.tsx
@@ -1,10 +1,15 @@
 type ButtonProps = {
   variant: 'primary' | 'secondary'
-} & React.ComponentProps<'button'>
+  children: string /* (2) Specify the children type, but we see the type of children is now an intersection of string and ReactNode */
+} & Omit<
+  React.ComponentProps<'button'>,
+  'children'
+> /* (3) So we use the Omit keyword to specify which we want to omit */
 
 export const CustomButton = ({ variant, children, ...rest }: ButtonProps) => {
   return (
     <button className={`class-with-${variant}`} {...rest}>
+      {/* (1) Let's say we want to restrict the children type to only strings... */}
       {children}
     </button>
   )
