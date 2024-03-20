82a9879a8df6154a9163db098a8cb9738f5c0313
Erick <12631133+erick207@users.noreply.github.com>
2024-03-20 00:50:43 -0300

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
 import './App.css'
-import { Text } from './components/polymorphic/Text'
 
 function App() {
-  return (
-    <div className='App'>
-      {/* We don't want them all to be divs, we want to be able to tell what component they will be with the 'as' prop, also 
-      to be able to pass specific props to each one*/}
-      <Text as='h1' size='lg'>
-        Heading
-      </Text>
-      <Text as='p' size='md'>
-        Paragraph
-      </Text>
-      <Text as='label' htmlFor='someId' size='sm' color='secondary'>
-        Label
-      </Text>
-    </div>
-  )
+  return <div className='App'></div>
 }
 
 export default App
