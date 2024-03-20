3bdc036f07fedf852a8e4d775b254c55fcd60c15
Erick <12631133+erick207@users.noreply.github.com>
2024-03-19 23:56:15 -0300

    Polymorphic Components: before

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import './App.css'
+import { Text } from './components/polymorphic/Text'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <Text size='lg'>Heading</Text>
+      <Text size='md'>Paragraph</Text>
+      <Text size='sm' color='secondary'>
+        Label
+      </Text>
+    </div>
+  )
 }
 
 export default App
