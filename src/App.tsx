e8c12ac15c170ee92f07de970107f35c036b5d7f
Erick <12631133+erick207@users.noreply.github.com>
2024-03-19 23:49:57 -0300

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import './App.css'
-import { CustomButton } from './components/html/Button'
 
 function App() {
-  return (
-    <div className='App'>
-      <CustomButton variant='primary' onClick={() => console.log('clicked ')}>
-        Primary Button
-      </CustomButton>
-    </div>
-  )
+  return <div className='App'></div>
 }
 
 export default App
